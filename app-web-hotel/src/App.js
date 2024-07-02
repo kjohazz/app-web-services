1015264eb78e891f8104d1151724ad84ef330ac5
kmendez <62451263+kjohazz@users.noreply.github.com>
2024-07-02 16:01:32 -0600

    new-vwesion

--- app-web-hotel/src/App.js
+++ app-web-hotel/src/App.js
@@ -1,20 +1,9 @@
 import React from 'react';
-import AdminPanel from './components/AdminPanel/AdminPanel';
-import Header from './components/Header/Header';
-//import Navigation from './components/Navigation/Navigation';
 
 function App() {
-  /*const homeLinks = [
-    { url: '/', text: 'Inicio' },
-    { url: '/tours', text: 'Tours' },
-    { url: '/contacto', text: 'Contacto' }
-  ];*/
-
   return (
     <div className="App">
-      <Header />
-
-      <AdminPanel />
+      Hello Word
     </div>
   );
 }
