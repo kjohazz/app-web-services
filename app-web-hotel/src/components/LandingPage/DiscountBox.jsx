e01748f1f161a880a041cbfb5c2eebd9b1abd9e5
kmendez <62451263+kjohazz@users.noreply.github.com>
2024-07-16 13:40:36 -0600

    style fix - code form and discount box
    modificando los estilos de Header, añadiendo modal para enlace de reservas, modificando estilo de el formulario de codigo y estilos de la caja de descuentos y añadiendo nuevas funcionalidades

--- app-web-hotel/src/components/LandingPage/DiscountBox.jsx
+++ app-web-hotel/src/components/LandingPage/DiscountBox.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './DiscountBox.module.css';
 
 function DiscountBox() {
+    const discountTexts = [
+        "Rio Negro, Tubing Adventure",
+        "Volcanic mud bath and hot springs",
+        "Horseback riding through the rainforest",
+    ];
+
+    const discountPercentages = [
+        "15% off",
+        "20% off",
+        "10% off",
+    ];
+
+    const [currentTextIndex, setCurrentTextIndex] = useState(0);
+    const discountTextsLength = useRef(discountTexts.length); // Usar useRef para almacenar la longitud
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentTextIndex(prevIndex => (prevIndex + 1) % discountTextsLength.current); // Usar la longitud desde useRef
+        }, 10000); // Cambiar cada 10 segundos (10000 ms)
+
+        return () => clearInterval(interval); // Limpiar el intervalo al desmontar
+    }, []);
+
     return (
         <div className={styles.discountBox}>
-            <p className={styles.discountText}>15% off</p>
-            <p className={styles.discountSubtext}>in your next adventure</p>
+            <h2 className={styles.discountTitle}>Tour</h2>
+            <p className={styles.discountSubtext}>{discountTexts[currentTextIndex]}</p>
+            <p className={styles.discountText}>{discountPercentages[currentTextIndex]}</p>
         </div>
     );
 }
