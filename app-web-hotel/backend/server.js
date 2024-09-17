1b1132e516fd689a88c39b4c652740cd3f26f595
kmendez <62451263+kjohazz@users.noreply.github.com>
2024-09-17 13:49:39 -0600

    prueba-mail

--- app-web-hotel/backend/server.js
+++ app-web-hotel/backend/server.js
@@ -32,13 +32,10 @@ app.use(cors({
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: false,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
-    },
-    tls: {
-        rejectUnauthorized: false
     }
 });
 
